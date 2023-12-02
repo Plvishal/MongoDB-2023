9905849ad22cc8197919290780dd43c3412f8c1c
Vishal <vishalpal1501@gmail.com>
2023-12-02 21:02:34 +0530

    User SignUp is done in this commit

--- 1. Connecting to MongoDB using NodeJS/src/config/mongodb.js
+++ 1. Connecting to MongoDB using NodeJS/src/config/mongodb.js
@@ -1,18 +1,20 @@
+import { MongoClient } from 'mongodb';
 
-import { MongoClient } from "mongodb";
-
-const url1 = "mongodb://localhost:27017";
+const url1 = 'mongodb://localhost:27017';
 // If the above url gives error (error may be caused due to IPv4/IPv6 configuration conflict), then try the url given below
-const url = "mongodb://127.0.0.1:27017/ecomdb";
+const url = 'mongodb://127.0.0.1:27017/ecomdb';
 
-const connectToMongoDB = ()=>{
-     MongoClient.connect(url)
-        .then(client=>{
-            console.log("Mongodb is connected");
-        })
-        .catch(err=>{
-            console.log(err);
-        })
-}
-
-export default connectToMongoDB;
+let client;
+export const connectToMongoDB = () => {
+  MongoClient.connect(url)
+    .then((clientInstance) => {
+      client = clientInstance;
+      console.log('Mongodb is connected');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+export const getDB = () => {
+  return client.db();
+};
--- 1. Connecting to MongoDB using NodeJS/src/features/user/user.model.js
+++ 1. Connecting to MongoDB using NodeJS/src/features/user/user.model.js
@@ -1,3 +1,5 @@
+import { getDB } from '../../config/mongodb.js';
+import { ApplicationError } from '../../error-handler/applicationError.js';
 export default class UserModel {
   constructor(name, email, password, type, id) {
     this.name = name;
@@ -7,23 +9,24 @@ export default class UserModel {
     this.id = id;
   }
 
-  static signUp(name, email, password, type) {
-    const newUser = new UserModel(
-      name,
-      email,
-      password,
-      type
-    );
-    newUser.id = users.length + 1;
-    users.push(newUser);
-    return newUser;
+  static async signUp(name, email, password, type) {
+    try {
+      // 1. Get the datbase
+      const db = getDB();
+      // 2.Get the collection
+      const collection = db.collection('users');
+      // 3.
+      const newUser = new UserModel(name, email, password, type);
+      // 4. Insert the document
+      await collection.insertOne(newUser);
+      return newUser;
+    } catch (error) {
+      throw new ApplicationError('Something went wrong,500');
+    }
   }
 
   static signIn(email, password) {
-    const user = users.find(
-      (u) =>
-        u.email == email && u.password == password
-    );
+    const user = users.find((u) => u.email == email && u.password == password);
     return user;
   }
 
